7a674b51ff5215aa4ab59c25d2e3b913c2cd44c0
techleadevelopers <pauloti.engenharia@gmail.com>
2026-07-18 22:53:11 -0300

    feat: update main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from pathlib import Path
+import sys
+
+sys.path.insert(0, str(Path(__file__).resolve().parent / "src"))
+
+from kyc_local_ai.app import main
+
+
+if __name__ == "__main__":
+    main()
